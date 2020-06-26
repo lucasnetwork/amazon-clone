d25e4e1a02d799e7e119552dbcd430b14dbda3a4
Lucas Santos Ribeiro <lucassr45@protonmail.com>
2020-06-26 13:47:13 -0300

    feat(styles): add font roboto

--- src/Components/Global/Styles.js
+++ src/Components/Global/Styles.js
@@ -1,7 +1,12 @@
 import {createGlobalStyle} from 'styled-components'
-
+import Roboto from '../../fonts/Roboto-Regular.ttf'
 const Global = createGlobalStyle`
+    @font-face{
+        font-family:'Roboto';
+        src:url(${Roboto});
+    }
     *{
+        font-family:'Roboto';
         margin:0;
         padding:0;
         box-sizing:border-box;
